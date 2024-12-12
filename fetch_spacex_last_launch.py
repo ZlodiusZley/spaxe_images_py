51b5594df108c3a404a0d75964fc4d5ed6417d5a
ZlodiusZley <136110833+ZlodiusZley@users.noreply.github.com>
2024-12-12 19:42:59 +0300

    исправил fetch_spacex_last_launch.py

--- fetch_spacex_last_launch.py
+++ fetch_spacex_last_launch.py
@@ -5,17 +5,18 @@ from datetime import datetime
 from save_tools import save_picture
 
 def main(): 
-    launch_id = "5eb87d42ffd86e000604b384"
-    folder = 'images'
+    parser = argparse.ArgumentParser(description="Данный файл скачивает картинки с сервиcа APOD NASA")
+    parser.add_argument('--folder', type=str, default='images', help='Введите название папки')
+    parser.add_argument('--launch', type=str, default='5eb87d42ffd86e000604b384', help='Введите айди запуска')
+    args = parser.parse_args
+    folder = args.folder
+    launch_id = args.folder
     url = f'https://api.spacexdata.com/v5/launches/{launch_id}'
     response = requests.get(url)
     response.raise_for_status()
     images_url = response.json()["links"]["flickr"]["original"] 
 
     for image in images_url:
-        response = requests.get(image)
-        response.raise_for_status()
-
         file_name = image.split("/")[-1]  
         save_picture(folder, url, file_name)
 
